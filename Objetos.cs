cb92ab5e34475492b30c092531648ad380d607df
Morgana Morais <morganamorais25@gmail.com>
2022-03-18 15:11:32 -0300

    first commit

---
+++ Objetos.cs
@@ -0,0 +1,20 @@
+
+    public ref struct Pessoa
+    {
+         public int Idade  { get; set; }
+         public string Nome { get; set; }
+         public  Endereco EnderecoPessoa { get; set; }
+    }
+    public  struct Endereco: Dados
+    {
+       public int Numero  { get; set; }
+       public string Logradouro { get; set; }
+        public string Cep { get; set; }
+        public string Cidade { get; set; }
+
+      }
+    public interface Dados
+    {
+       public int Numero  { get; set; }
+        public string Logradouro { get; set; }  
+    }
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+  using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+    p1.Nome = "Ricardo";
+    p1.Idade = 30;
+    p1.EnderecoPessoa = new Endereco()
+    {
+        Logradouro = "Ru teste",
+        Numero = 300,
+        Cep = "000000",
+        Cidade = "São Paulo",
+
+    };
+             WriteLine("Fim");
+
+
